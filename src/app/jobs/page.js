a5c2927558cfa41e207ed46005e866f07b672b61
brittLiban <britt.liban@student.greenriver.edu>
2025-06-10 15:26:40 -0700

    Making a page that will be for showing all the recent jobs!

--- src/app/jobs/page.js
+++ src/app/jobs/page.js
@@ -3,12 +3,3 @@
 import { useEffect, useState } from 'react';
 import { Typography, Box, Button, CircularProgress, Stack } from '@mui/material';
 import Link from 'next/link';
-
-export default function runJob() {
-
-    return (
-        <div>
-            
-        </div>
-    )
-}
