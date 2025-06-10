a0d2ce471c7f0cd2c2fed29311a8ab3f865f6743
brittLiban <britt.liban@student.greenriver.edu>
2025-06-10 15:57:02 -0700

    AI USED HERE - helped write the logger function
    THAT WORKS ON THE FIRST TIME ASHFLAKHFALKFHJFIAS

--- src/app/jobs/page.js
+++ src/app/jobs/page.js
@@ -3,3 +3,15 @@
 import { useEffect, useState } from 'react';
 import { Typography, Box, Button, CircularProgress, Stack } from '@mui/material';
 import Link from 'next/link';
+
+// How its going to work, its going to poll all the recent jobs that have been done and when someone clicks on theirs it will show its csv as well
+
+//I also want the ability for someone to do it by getting the time and date
+
+export default function jobsPage(){
+
+    fetch()
+    .then(res => {
+        //put all the entries into a map
+    })
+}
