ae64a05e45d5d3b4210f257ae906b5d7ebe77567
brittLiban <britt.liban@student.greenriver.edu>
2025-06-10 16:18:49 -0700

    Importing stuff for a table

--- src/app/jobs/page.js
+++ src/app/jobs/page.js
@@ -1,8 +1,11 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { Typography, Box, Button, CircularProgress, Stack } from '@mui/material';
-import Link from 'next/link';
+import { Typography, Box, Button, CircularProgress, Stack, dividerClasses } from '@mui/material';
+import {
+  Box, Typography, Table, TableBody, TableCell, TableContainer,
+  TableHead, TableRow, Paper
+} from '@mui/material';
 
 // How its going to work, its going to poll all the recent jobs that have been done and when someone clicks on theirs it will show its csv as well
 
@@ -10,21 +13,23 @@ import Link from 'next/link';
 
 export default function JobsPage() {
 
-const [jobs, setJobs] = useState([]);
-  useEffect(() => {
-    const getJobs = async () => {
-      try {
-        const res = await fetch('http://localhost:3001/jobs');
-        const data = await res.json();
-        setJobs(data);
-        console.log('Fetched jobs:', data);
-      } catch (err) {
-        console.error('Error fetching jobs:', err);
-      }
-    };
+    const [jobs, setJobs] = useState([]);
+    useEffect(() => {
+        const getJobs = async () => {
+            try {
+                const res = await fetch('http://localhost:3001/jobs');
+                const data = await res.json();
+                setJobs(data);
+                console.log('Fetched jobs:', data);
+            } catch (err) {
+                console.error('Error fetching jobs:', err);
+            }
+        };
 
-    getJobs(); 
-  }, []);// Run once after initial render
+        getJobs();
+    }, []);// Run once after initial render
 
- 
+    return (
+       <div>jobs</div>
+    );
 }
