f29ac6f6e2c2e1fcccf0e5442e93244f48ee1171
brittLiban <britt.liban@student.greenriver.edu>
2025-06-10 17:23:37 -0700

    structuring was wrong. -1 to nextJs

---
+++ src/app/results/[jobId]/page.js
@@ -0,0 +1,42 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import {
+  Box,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper
+} from '@mui/material';
+
+import { useParams } from 'next/navigation';
+
+
+// Lets put the results in a table again. Also make it downloadable
+
+export default function getJobCsv(){
+    const params = useParams(); //useParams() from Next.js App Router
+  const { jobId } = params;
+
+    const [id, setId] = useState([]);
+    useEffect(() => {
+        const getCsv = async () => {
+            try {
+                const res = await fetch(`http://localhost:3001/csvjson/${jobId}`);
+                const data = await res.json();
+                setId(data);
+                console.log('Fetched csv:', data);
+            } catch (err) {
+                console.error('Error fetching jobs:', err);
+            }
+        };
+
+        getCsv();
+    }, [id]);// Run once after initial render
+
+}
